eb42a2466fd0700c7856cb6bbaf75bafc8f7931c
yasminekhelil <67135169+yasminekhelil@users.noreply.github.com>
2020-08-13 23:32:40 -0400

    Update picard.py

--- outils/picard.py
+++ outils/picard.py
@@ -3,14 +3,8 @@ import re
 import sys
 import glob
 import os
-import logging
 import argparse
-from janis_bioinformatics.tools.gatk4.markduplicates.versions import Gatk4MarkDuplicates_4_1_4
-
-# Initialise the logger
-log = logging.getLogger(__name__)
-
-
+import subprocess 
 
 def init():
     parser = argparse.ArgumentParser(
@@ -20,11 +14,6 @@ def init():
     parser.add.argument('-r', '--markduplicates',dest ='picard-markduplicates ', default = None, help='Indexed')
     args = parser.parse_args()
     return args
-
-
-
-
-
 
 
 def print2file(cmd, filename):
@@ -47,7 +36,7 @@ def markDuplicates(sample, output, read1, read2=None, index =None,):
    cmd +=  '-jar $MRKDUP'
    cmd += "{} {} {}>{}.bam\n".format(index, read1, read2, output)
 
-   cmd +=  'INPUT=$OUTDIR/${read1}_${read2}_${output}-s.bam '
+   cmd +=  'INPUT=$OUTDIR/${read1}_${read2}_${output}-s.bam '.format(index, read1, read2, output)
    cmd +=  'OUTPUT=$OUTDIR/${read1}_${read2}_${output}-smd.bam '
    cmd +=  'METRICS_FILE=$OUTDIR/${read1}_${read2}_${output}-smd.metrics'
    cmd +=  'AS=TRUE '
@@ -56,6 +45,30 @@ def markDuplicates(sample, output, read1, read2=None, index =None,):
    cmd +=   'REMOVE_DUPLICATES=TRUE'
    cmd +=  'samtools sort $OUTDIR/${read1}_${read2}_${output}-smd.bam $OUTDIR/${read1}_${read2}_${output}-smds'
    cmd += 'samtools index $OUTDIR/${read1}_${read2}_${output}-smds.bam'
-   cmd +=  'samtools flagstat $OUTDIR/${read1}_${read2}_${output}-smds.bam > $OUTDIR/${read1}_${read2}_${output}-smds.flagstat'
+   cmd +=  'samtools flagstat $OUTDIR/${read1}_${read2}_${output}-smds.bam > $OUTDIR/${read1}_${read2}_${output}-smds.flagstat'.format(index, read1, read2, output)
    name = 'markduplicates'+ sample+".bam"
    return cmd , name
+
+
+
+if __name__ == '__main__':
+    args = init()
+    pattern = re.compile('.*/(.+)_([A-Z0-9]+)_([LS]\d+)_R1(_001)*.bam*')
+    sample_list = set()
+    for bam in glob.glob(os.path.join(args.source, "*markduplicates.bam*"))) +
+glob.glob(os.path.join(args.source,"*markduplicates.bam*")):
+        matched = pattern(bam)
+        if matched :
+            sample = matched.group(1)
+
+        else :
+            splitor = 'markduplicates.bam' if 'markduplicates.bam' in fastq else 'markduplicates.bam'
+            sample = os.path.basename(bam).split(splitor)[0]
+        sample_list.add(sample)
+    for sample in sample_list:
+        cmd,script = bam_sample(source=args.source,
+outputdir=args.output,samplename=sample, index=args.bamindex)
+
+        print2file(cmd,script)
+        job_id = subprocess.check_output('sbatch {}'.format(script).split())
+        print(job_id.decode("utf-8").strip())
